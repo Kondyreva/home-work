b3d0470287c65ebfb983fc48e680b7bc69b5f71c
Kondyreva <Kondyreva.T.Vi@sberbank.ru>
2021-11-22 22:59:51 +0300

    delete import from main method

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -1,13 +1,5 @@
 package com.sbrf.reboot;
 
-import com.sbrf.reboot.dto.Account;
-import com.sbrf.reboot.utils.AccountUtils;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
