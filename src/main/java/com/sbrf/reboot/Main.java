fcc5c14b8ab8721df9e967b352f95240fe9aa9de
Kondyreva <93218851+Kondyreva@users.noreply.github.com>
2021-11-22 22:58:44 +0300

    Update Main.java

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -4,9 +4,5 @@ import com.sbrf.reboot.calculator.Calculator;
 
 public class Main {
     public static void main(String[] args) {
-        Integer a = 5;
-        Integer b = 10;
-        Integer result = Calculator.getAddition(5,10);
-        System.out.println(result.toString());
     }
 }
