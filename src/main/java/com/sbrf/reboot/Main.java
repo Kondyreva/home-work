d958d664c24f9cd4f7f88fb42c398bda7583a533
Kondyreva <Kondyreva.T.Vi@sberbank.ru>
2021-11-22 22:58:02 +0300

    delete all from main method

--- src/main/java/com/sbrf/reboot/Main.java
+++ src/main/java/com/sbrf/reboot/Main.java
@@ -10,16 +10,6 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Account> accounts = new ArrayList<Account>() {{
-            add(Account.builder().id(1L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(1L).createDate(LocalDate.now()).balance(BigDecimal.ONE).build());
-            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(3)).balance(BigDecimal.TEN).build());
-            add(Account.builder().id(3L).createDate(LocalDate.now().minusDays(1)).balance(BigDecimal.ZERO).build());
-            add(Account.builder().id(2L).createDate(LocalDate.now()).balance(BigDecimal.TEN).build());
-        }};
 
-        AccountUtils.sortedByIdDateBalance(accounts);
-
-        System.out.println(accounts.toString());
     }
 }
