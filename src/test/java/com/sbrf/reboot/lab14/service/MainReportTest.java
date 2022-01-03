546cfe044f88bc979992a1d86f1f74cca9e88ff9
Kondyreva <Kondyreva.T.Vi@sberbank.ru>
2022-01-03 20:24:27 +0300

    lesson-14

---
+++ src/test/java/com/sbrf/reboot/lab14/service/MainReportTest.java
@@ -0,0 +1,68 @@
+package com.sbrf.reboot.lab14.service;
+
+import com.sbrf.reboot.lab14.dto.Account;
+import com.sbrf.reboot.lab14.dto.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MainReportTest {
+    @InjectMocks
+    private MainReportImpl operation;
+
+    List<Customer> newCustomer;
+
+    @BeforeEach
+    void init() {
+        List<Account> accountFirstList = new ArrayList<>();
+        accountFirstList.add(new Account(100.0, "RUB", LocalDate.of(2021, 7, 1)));
+        accountFirstList.add(new Account(200.0, "RUB", LocalDate.of(2021, 7, 5)));
+        accountFirstList.add(new Account(300.0, "RUB", LocalDate.of(2021, 7, 6)));
+        accountFirstList.add(new Account(400.0, "EUR", LocalDate.of(2021, 7, 7)));
+
+        List<Account> accountSecondList = new ArrayList<>();
+        accountSecondList.add(new Account(2000.0, "RUB", LocalDate.of(2021, 7, 10)));
+        accountSecondList.add(new Account(5000.0, "RUB", LocalDate.of(2021, 8, 1)));
+        accountSecondList.add(new Account(3500.0, "USD", LocalDate.of(2021, 7, 10)));
+
+        List<Account> accountThirdList = new ArrayList<>();
+        accountThirdList.add(new Account(155.55, "RUB", LocalDate.of(2021, 7, 15)));
+        accountThirdList.add(new Account(355.5, "RUB", LocalDate.of(2021, 7, 15)));
+
+        newCustomer = new ArrayList<>();
+        newCustomer.add(new Customer(18, "First", accountFirstList));
+        newCustomer.add(new Customer(20, "Second", accountSecondList));
+        newCustomer.add(new Customer(31, "Third", accountThirdList));
+    }
+
+    @Test
+    void getTotalsWithCompletableFuture() throws ExecutionException, InterruptedException {
+        CompletableFuture<Double> balanceByCompletableFuture = operation.getTotalsWithCompletableFuture(newCustomer.stream());
+        Double result = balanceByCompletableFuture.get();
+
+        assertEquals(2500.0,result);
+    }
+
+    @Test
+    void getTotalsWithReact() {
+        Mono<Double> balanceByReact = operation.getTotalsWithReact(newCustomer.stream());
+        balanceByReact.subscribeOn(Schedulers.parallel());
+        Double result = balanceByReact.block();
+
+        assertEquals(2500.0,result);
+    }
+
+}
